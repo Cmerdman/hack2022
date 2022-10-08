fc957f8696bcc46b3563c4d63bf4aab16dd726b9
RSunderhaft <sunderhaft.2@buckeyemail.osu.edu>
2022-10-08 17:09:01 -0400

    New app

---
+++ app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotly.express as px
+import dash
+from dash import Dash, html, dcc, Input, Output
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='AEP'), 
+    html.Div(children=[
+        html.Label('Dropdown'),
+        dcc.Dropdown(['Total', 'Station A', 'Station B', "Station C", "Station D", "Station E", "Station F", "Station G", "Station H", "Station I", "Station J"], "Total")
+    ])
+    ,
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
